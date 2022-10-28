e167cdad9cad7ed912bb9af7f9b3b3c3fca342d1
ortizjulian <julian.ortixs@gmail.com>
2022-10-28 16:34:10 -0500

    Signin

--- lib/vistas/signin.dart
+++ lib/vistas/signin.dart
@@ -16,7 +16,60 @@ class signin extends StatelessWidget {
           color: Color(0xFF031630),
         ),
         child: Column(
-          children: [],
+          children: [
+            Spacer(),
+            Row(
+              children: [
+                Spacer(),
+                Image.network(
+                  'https://assets.stickpng.com/images/585e4beacb11b227491c3399.png',
+                  fit: BoxFit.contain,
+                  height: 142,
+                  width: 152,
+                ),
+                Spacer()
+              ],
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            SizedBox(width: 257, child: campo("Ingrese un usuario")),
+            Spacer(),
+            SizedBox(width: 257, child: campo("Ingrese un contraseña")),
+            Spacer(
+              flex: 2,
+            ),
+            SizedBox(width: 257, child: campo("Ingrese un correo")),
+            Spacer(
+              flex: 2,
+            ),
+            SizedBox(width: 257, child: campo("Ingrese su nombre completo")),
+            Spacer(
+              flex: 2,
+            ),
+
+            SizedBox(
+              child: button(() {}, "Registrarse"),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Spacer(),
+                Text(
+                  '¿Ya tienes cuenta?',
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Spacer()
+              ],
+            ),
+            Spacer(),
+          ],
         ),
       )),
     );
